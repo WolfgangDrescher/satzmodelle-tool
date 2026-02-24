544b592bd76fa83ee12e275afd5df7a5a27ac820
percussionfloh <florian.moldaschl@web.de>
2026-02-24 16:48:57 +0100

    add pinia/nuxt to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: false },
-    modules: ['@nuxt/content', '@nuxt/ui',
+    modules: ['@nuxt/content', '@nuxt/ui',  '@pinia/nuxt',
         [
             '@nuxtjs/i18n',
             {
