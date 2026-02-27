b03fb5d216fe521b6f040862c1ca7f39f840d871
percussionfloh <florian.moldaschl@web.de>
2026-02-27 21:40:11 +0100

    update karussell.vue

--- app/pages/karussell.vue
+++ app/pages/karussell.vue
@@ -1,1 +1,13 @@
-<template></template>
+<script setup>
+ const { t } = useI18n();
+</script>
+
+<template>
+    <UContainer>
+        <div class="flex flex-col gap-8">
+            <h1 class="text-3xl font-bold">Karussell</h1>
+        <VerovioCanvas view-mode="horizontal" url="/krn/karussell.krn" :scale="35" :page-margin="20" />
+        <!-- <MidiPlayer url="/krn/karussell.krn" class="text-2xl"/> -->
+        </div>
+    </UContainer>
+</template>
