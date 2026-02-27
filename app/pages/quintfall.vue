c0b7dcf758ff240f400fb2f09dd46080849e3660
percussionfloh <florian.moldaschl@web.de>
2026-02-27 20:40:55 +0100

    update quintfall.vue

--- app/pages/quintfall.vue
+++ app/pages/quintfall.vue
@@ -1,1 +1,14 @@
-<template></template>
+ <script setup>
+ const { t } = useI18n();
+</script>
+
+<template>
+    <UContainer>
+        <div class="flex flex-col gap-8">
+            <h1 class="text-3xl font-bold">Quintfall</h1>
+        <h2>Diatonischer Quintfall, mit Gegenschrittmodell 3-6 in Außenstimmen</h2>
+        <VerovioCanvas view-mode="horizontal" url="/krn/quintfall-dia-3-bass.krn" :scale="35" :page-margin="20" />
+        <!-- <MidiPlayer url="/krn/quintfall-dia-3-bass.krn" class="text-2xl"/> -->
+        </div>
+    </UContainer>
+</template>
