804f576dadb4e6a80e65efc9463d42ab9fccc47e
percussionfloh <florian.moldaschl@web.de>
2026-02-27 21:59:56 +0100

    quintfall.vue update

--- app/pages/quintfall.vue
+++ app/pages/quintfall.vue
@@ -1,4 +1,4 @@
- <script setup>
+<script setup>
  const { t } = useI18n();
 </script>
 
@@ -6,9 +6,31 @@
     <UContainer>
         <div class="flex flex-col gap-8">
             <h1 class="text-3xl font-bold">Quintfall</h1>
-        <h2>Diatonischer Quintfall, mit Gegenschrittmodell 3-6 in Außenstimmen</h2>
-        <VerovioCanvas view-mode="horizontal" url="/krn/quintfall-dia-3-bass.krn" :scale="35" :page-margin="20" />
-        <!-- <MidiPlayer url="/krn/quintfall-dia-3-bass.krn" class="text-2xl"/> -->
+        </div>
+        <div class="flex flex-col gap-8">
+            <h2>Diatonischer Quintfall, mit Gegenschrittmodell 3-6 in Außenstimmen</h2>
+            <VerovioCanvas view-mode="horizontal" url="/krn/quintfall-dia-3-bass.krn" :scale="35" :page-margin="20" />
+            <!-- <MidiPlayer url="/krn/quintfall-dia-3-bass.krn" class="text-2xl"/> -->
+        </div>
+        <div class="flex flex-col gap-8">
+            <h2>Diatonischer Quintfall mit Grundakkorden</h2>
+            <VerovioCanvas view-mode="horizontal" url="/krn/quintfall-dia-3-5.krn" :scale="35" :page-margin="20" />
+            <!-- <MidiPlayer url="/krn/quintfall-dia-3-5.krn" class="text-2xl"/> -->
+        </div>
+            <div class="flex flex-col gap-8">
+            <h2>Diatonischer Quintfall mit Septakkorden</h2>
+            <VerovioCanvas view-mode="horizontal" url="/krn/quintfall-dia-7.krn" :scale="35" :page-margin="20" />
+            <!-- <MidiPlayer url="/krn/quintfall-dia-7.krn" class="text-2xl"/> -->
+        </div>
+        <div class="flex flex-col gap-8">
+            <h2>Realer Quintfall mit Grundakkorden</h2>
+            <VerovioCanvas view-mode="horizontal" url="/krn/quintfall-real-3-5.krn" :scale="35" :page-margin="20" />
+            <!-- <MidiPlayer url="/krn/quintfall-real-3-5.krn" class="text-2xl"/> -->
+        </div>
+            <div class="flex flex-col gap-8">
+            <h2>Realer Quintfall mit Septakkorden</h2>
+            <VerovioCanvas view-mode="horizontal" url="/krn/quintfall-real-7.krn" :scale="35" :page-margin="20" />
+            <!-- <MidiPlayer url="/krn/quintfall-real-7.krn" class="text-2xl"/> -->
         </div>
     </UContainer>
 </template>
