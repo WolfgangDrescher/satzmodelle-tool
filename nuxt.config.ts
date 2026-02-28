c8a36595250f6d8cd92f2422dbb47a7a366bb9bc
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-02-28 19:44:17 +0100

    Remove doubled i18n config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: false },
-    modules: ['@nuxt/content', '@nuxt/ui',  '@pinia/nuxt',
-        [
-            '@nuxtjs/i18n',
-            {
-                strategy: 'prefix_except_default',
-                locales: [
-                    { code: 'de', language: 'de-DE', file: 'de.yaml', dir: 'ltr' },
-                ],
-                defaultLocale: 'de',
-                langDir: 'locales/',
-            },
-        ],
-    ],
+    modules: ['@nuxt/content', '@nuxt/ui',  '@pinia/nuxt','@nuxtjs/i18n'],
     css: ['~/assets/main.css'],
     i18n: {
         strategy: 'prefix_except_default',
